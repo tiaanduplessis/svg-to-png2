33b99bcdc4657ffdbf34e277547abd35f931518d
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2017-11-08 00:57:49 +0200

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,42 +10,12 @@ module.exports = function(grunt) {
 		},
 		qunit: {
 			files: ['test/phantom/index.html']
-		},
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			lib: {
-				src: ['lib/**/*.js', '!lib/rsvp.js']
-			},
-			test: {
-				src: ['test/**/*.js', '!test/phantom/vendor/*.js', '!test/phantom/phantomtest.js']
-			},
-		},
-		watch: {
-			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
-			},
-			lib: {
-				files: '<%= jshint.lib.src %>',
-				tasks: ['jshint:lib', 'nodeunit']
-			},
-			test: {
-				files: '<%= jshint.test.src %>',
-				tasks: ['jshint:test', 'nodeunit']
-			},
-		},
+		}
 	});
 
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.task.registerTask( 'phantom-unit', 'A task to run the phantom tests', function(){
 		var done = this.async();
@@ -56,6 +26,6 @@ module.exports = function(grunt) {
 	});
 
 	// Default task.
-	grunt.registerTask('default', ['jshint', 'nodeunit', 'phantom-unit']);
+	grunt.registerTask('default', ['nodeunit', 'phantom-unit']);
 
 };
